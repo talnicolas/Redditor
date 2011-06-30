70ee50569ffe8e57dec31e09d393fd607aade2cd
Nicolas Tallineau <nicolas.tallineau@gmail.com>
2011-06-30 11:54:45 -0700

    Added CommandUndoable.java doc.

--- src/inf5153/rEdditooooooor/Controler/impl/CommandUndoable.java
+++ src/inf5153/rEdditooooooor/Controler/impl/CommandUndoable.java
@@ -3,18 +3,27 @@ package rEdditooooooor.Controler.impl;
 import rEdditooooooor.Controler.IEditorCommand;
 import rEdditooooooor.Model.TextConcrete;
 
+/**
+ * Undoable commands are commands that affects the state of the model
+ */
 public abstract class CommandUndoable extends CommandManager implements IEditorCommand {
 
 	protected CommandManager cM;
 	protected TextConcrete text;
 	private String state;
 	
+	/**
+	 * Default constructor
+	 */
 	public CommandUndoable() {
 		this.text = TextConcrete.getInstance();
 		this.cM = CommandManager.getInstance();
 		this.state = this.text.getState();
 	}
 	
+	/**
+	 * @see rEdditooooooor.Controler.impl.CommandManager.undo()
+	 */
 	public void undo() {
 		String previousState = this.state;
 		this.state = this.text.getState();
@@ -22,6 +31,9 @@ public abstract class CommandUndoable extends CommandManager implements IEditorC
 		this.text.setState(previousState);	
 	}
 	
+	/**
+	 * @see rEdditooooooor.Controler.impl.CommandManager.redo()
+	 */
 	public void redo() {
 		String previousState = this.state;
 		this.state = this.text.getState();

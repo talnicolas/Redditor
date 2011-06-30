c7df19dc31e4d4571f0d01db91edecdbc38d74af
Nicolas Tallineau <nicolas.tallineau@gmail.com>
2011-06-30 11:35:23 -0700

    Added ClipBoard.java doc

--- src/inf5153/rEdditooooooor/Model/ClipBoard.java
+++ src/inf5153/rEdditooooooor/Model/ClipBoard.java
@@ -4,22 +4,33 @@ package rEdditooooooor.Model;
 
 
 /**
-Save a selected text while the cut and copy operations
+ * Save a selected text while the cut and copy operations
  */
 public class ClipBoard 
 {
    private String selection;
-   
+
+   /**
+    * Default Constructor
+    */
    public ClipBoard() 
    {    
 	   selection = null;
    }
    
+   /**
+    * Save a selection previously selected by the user
+    * 
+    * @param aSelection the selection to be saved
+    */
    public void save(String aSelection) 
    {
 	   this.selection = aSelection;
    }
    
+   /**  
+    * @return the selection that has been previously saved
+    */
    public String getSelection() 
    {
 	   if(this.selection != null){

4906382e90c9bee98f2d8dc497fa0e6734b00871
Nicolas Tallineau <nicolas.tallineau@gmail.com>
2011-06-30 11:22:33 -0700

    Added IEditorView.java doc

--- src/inf5153/rEdditooooooor/View/IEditorView.java
+++ src/inf5153/rEdditooooooor/View/IEditorView.java
@@ -4,10 +4,13 @@ package rEdditooooooor.View;
 
 
 /**
-Interface that allows developers to create their own custom UI for this 
-application
+ * Interface that allows developers to create their own custom UI for this 
+ * application
  */
 public interface IEditorView 
 {   
+   /**
+    * Get the state of the Model (TextConcrete) and update the view for the user.
+    */
    public void update();
 }
